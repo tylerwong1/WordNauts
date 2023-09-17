43cb88e8b9ebf98b2efba9c3635e83fd4e11ad93
tylerwong1 <tjwong1@gmail.com>
2023-09-16 20:24:28 -0400

    updated server side

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,20 +1,32 @@
 import React from "react";
-
-const Navbar = () => {
-    console.log("test")
-    return (
-      <nav className="navbar">
-        <div className="navbar-logo">
-          <a href="/">Logo</a>
-        </div>
-        <ul className="navbar-links">
-          <li><a href="/">Home</a></li>
-          <li><a href="/about">About</a></li>
-          <li><a href="/services">Services</a></li>
-          <li><a href="/contact">Contact</a></li>
-        </ul>
-      </nav>
-    );
-  };
+function Navbar() {
+  console.log("test");
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <a href="#">
+          <img className="navbar-logo" src="../public/logo.png"></img>
+        </a>
+      </div>
+      <ul className="navbar-links">
+        <li>
+          <a className="navbar-link" href="/">
+            Home
+          </a>
+        </li>
+        <li>
+          <a className="navbar-link" href="/leaderboard">
+            Leaderboard
+          </a>
+        </li>
+        <li>
+          <a className="navbar-link" href="/race">
+            Race
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Navbar;
